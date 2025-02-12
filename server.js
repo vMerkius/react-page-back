060699ca2c79cc2df257eddd245660ca7c698aea
Mateusz Rojek <106780153+vMerkius@users.noreply.github.com>
2025-02-12 23:56:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Works</h1>");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
